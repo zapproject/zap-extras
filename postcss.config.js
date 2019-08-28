2e61d8432f0c64902c7557a60624b7adc45c6d74
Pavel B <pavel.b@smartum.pro>
2019-08-28 18:03:45 +0300

    fix css modules classes included into pagination

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,10 @@
 module.exports = (ctx) => ({
-  plugins: [
-    require('postcss-modules')({
+	plugins: [
+		require('postcss-modules')({
 			getJSON: ctx.extractModules || (() => {}),
 		}),
 		require('postcss-url')({
 			url: 'copy',
 		}),
-  ],
+	],
 });
